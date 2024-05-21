0eb2ad46965478e7f8ef1063c5987b982195f2f2
laggedskapari <es.rajatyadav@gmail.com>
2024-05-21 15:52:46 +0530

    updated logs entity.

--- packages/logs/lib/src/entity/logs_log_entity.dart
+++ packages/logs/lib/src/entity/logs_log_entity.dart
@@ -5,27 +5,21 @@ class LogsLogEntity {
   final DateTime logCreatedOn;
   final int logDuration;
   final double successRate;
-  final int allowedSkips;
-  final int usedSkips;
   final bool isSuccess;
-  final bool isStrict;
   final DateTime logEndDate;
   final DateTime logCompletedOn;
   final DateTime logInitDate;
 
   const LogsLogEntity({
-    required this.isStrict,
     required this.logCompletedOn,
     required this.logEndDate,
     required this.firebaseLogId,
     required this.logId,
     required this.logTitle,
     required this.logCreatedOn,
-    required this.usedSkips,
     required this.isSuccess,
     required this.logDuration,
     required this.successRate,
-    required this.allowedSkips,
     required this.logInitDate,
   });
 
@@ -35,12 +29,9 @@ class LogsLogEntity {
       'logId': logId,
       'logTitle': logTitle,
       'logCreatedOn': logCreatedOn,
-      'usedSkips': usedSkips,
       'isSuccess': isSuccess,
       'logDuration': logDuration,
       'successRate': successRate,
-      'allowedSkips': allowedSkips,
-      'isStrict': isStrict,
       'logEndDate': logEndDate,
       'logCompleted': logCompletedOn, 
       'logInitDate': logInitDate,
@@ -50,18 +41,15 @@ class LogsLogEntity {
   static LogsLogEntity fromFirestoreDocument(Map<String, dynamic> doc) {
     return LogsLogEntity(
       logInitDate: doc['logInitDate'],
-      isStrict: doc['isStrict'],
       logEndDate: doc['logEndDate'],
       logCompletedOn: doc['logCompletedOn'],
       firebaseLogId: doc['firebaseLogId'],
       logId: doc['logId'],
       logTitle: doc['logTitle'],
       logCreatedOn: doc['logCreatedOn'],
-      usedSkips: doc['usedSkips'],
       isSuccess: doc['isSuccess'],
       logDuration: doc['logDuration'],
       successRate: doc['successRate'],
-      allowedSkips: doc['allowedSkips'],
     );
   }
 }
