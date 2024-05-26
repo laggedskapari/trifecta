a9687058d9e929cd63fffa51543d38231fb49f2d
laggedskapari <es.rajatyadav@gmail.com>
2024-05-26 08:30:51 +0530

    added Authenticated Navvigation bar.

---
+++ lib/presentation/components/authenticated/trifecta_navigation_bar.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class TrifectaNavigationBar extends StatefulWidget {
+  const TrifectaNavigationBar({super.key, required this.changeScreen});
+
+  final void Function(int index) changeScreen;
+
+  @override
+  State<TrifectaNavigationBar> createState() => _TrifectaNavigationBar();
+}
+
+class _TrifectaNavigationBar extends State<TrifectaNavigationBar> {
+  int currentIndex = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    List<String> screens = ['CROSS', 'LOGS', 'FOCUS'];
+    double displayWidth = MediaQuery.of(context).size.width;
+    double displayHeight = MediaQuery.of(context).size.height;
+
+    return Container(
+      alignment: Alignment.center,
+      height: displayHeight * .10,
+      margin: EdgeInsets.symmetric(
+        horizontal: displayWidth * .05,
+      ),
+      child: ListView.builder(
+        itemBuilder: (context, index) => InkWell(
+          onTap: () {
+            setState(() {
+              currentIndex = index;
+              widget.changeScreen(index);
+              HapticFeedback.lightImpact();
+            });
+          },
+          splashColor: Colors.transparent,
+          highlightColor: Colors.transparent,
+          child: Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: Text(
+              screens[index],
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight:
+                    index == currentIndex ? FontWeight.w900 : FontWeight.normal,
+                color: index == currentIndex
+                    ? Theme.of(context).colorScheme.primary
+                    : Theme.of(context).colorScheme.secondary,
+              ),
+            ),
+          ),
+        ),
+        shrinkWrap: true,
+        itemCount: screens.length,
+      ),
+    );
+  }
+}
