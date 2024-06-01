9b35973c46f794737347edf738a08a975b22ee5c
laggedskapari <es.rajatyadav@gmail.com>
2024-06-01 14:05:58 +0530

    fixed bug.
    fixed typo of signUp bloc to signIn bloc.

--- lib/presentation/screens/authentication/authentication_screen.dart
+++ lib/presentation/screens/authentication/authentication_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:trifecta/bloc/SignInBloc/sign_in_bloc.dart';
 import 'package:trifecta/bloc/SignUpBloc/sign_up_bloc.dart';
 import 'package:trifecta/bloc/TrifectaAuthenticationBloc/trifecta_authentication_bloc.dart';
 import 'package:trifecta/presentation/components/authentication/authentication_navigation_bar.dart';
@@ -37,10 +38,11 @@ class _AuthenticationScreenState extends State<AuthenticationScreen> {
             ),
           ),
           BlocProvider(
-            create: (context) => SignUpBloc(
-                trifectaAuthenticationRepository: context
-                    .read<TrifectaAuthenticationBloc>()
-                    .trifectaAuthenticationRepository),
+            create: (context) => SignInBloc(
+              trifectaAuthenticationRepository: context
+                  .read<TrifectaAuthenticationBloc>()
+                  .trifectaAuthenticationRepository,
+            ),
           ),
         ],
         child: <Widget>[
