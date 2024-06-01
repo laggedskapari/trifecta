823a62e92083a3073f3276d81f2de3ff81031ff4
laggedskapari <es.rajatyadav@gmail.com>
2024-06-01 13:02:22 +0530

    added Bloc Builder.

--- lib/trifecta.dart
+++ lib/trifecta.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:trifecta/bloc/SignInBloc/sign_in_bloc.dart';
+import 'package:trifecta/bloc/TrifectaAuthenticationBloc/trifecta_authentication_bloc.dart';
 import 'package:trifecta/presentation/color_scheme/trifecta_dark.dart';
+import 'package:trifecta/presentation/screens/authenticated/authenticated_screen.dart';
 import 'package:trifecta/presentation/screens/authentication/authentication_screen.dart';
 import 'package:trifecta_authentication/trifecta_authentication_repository.dart';
 
@@ -16,10 +19,34 @@ class Trifecta extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiRepositoryProvider(
       providers: [
+        RepositoryProvider(
+          create: (context) => TrifectaAuthenticationBloc(
+            trifectaAuthenticationRepository: trifectaAuthenticationRepository,
+          ),
+        ),
       ],
       child: MaterialApp(
         theme: trifectaDark,
-        home: const AuthenticationScreen(),
+        home: BlocBuilder<TrifectaAuthenticationBloc,
+            TrifectaAuthenticationState>(
+          builder: (context, state) {
+            if (state.status == TrifectaAuthenticationStatus.authenticated) {
+              return MultiBlocProvider(
+                providers: [
+                  BlocProvider<SignInBloc>(
+                    create: (context) => SignInBloc(
+                      trifectaAutheticationRepository: context
+                          .read<TrifectaAuthenticationBloc>()
+                          .trifectaAuthenticationRepository,
+                    ),
+                  ),
+                ],
+                child: const AuthenticatedScreen(),
+              );
+            }
+            return const AuthenticationScreen();
+          },
+        ),
       ),
     );
   }
