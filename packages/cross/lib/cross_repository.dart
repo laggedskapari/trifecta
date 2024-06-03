fbb35e471c8b490d50f825933308369f89afcd96
laggedskapari <es.rajatyadav@gmail.com>
2024-06-03 18:38:04 +0530

    added cross repository.

---
+++ packages/cross/lib/cross_repository.dart
@@ -0,0 +1,8 @@
+library cross_repository_repository;
+
+export 'src/entities/entities.dart';
+export 'src/model/models.dart';
+export 'src/cross_task_repository.dart';
+export 'src/cross_tasklist_repository.dart';
+export 'src/cross_task_repository_implementation.dart';
+export 'src/cross_tasklist_repository_implementation.dart';
