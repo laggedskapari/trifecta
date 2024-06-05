59c99aa1609052f754119e24c4372a2d55db5756
laggedskapari <es.rajatyadav@gmail.com>
2024-06-05 19:57:13 +0530

    added field.
    added firebaseTaskListId field to create new task in tasklist.

--- lib/presentation/components/authenticated/cross/new_task_form.dart
+++ lib/presentation/components/authenticated/cross/new_task_form.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:trifecta/bloc/Cross/TaskBloc/task_bloc.dart';
 import 'package:trifecta/presentation/components/authentication/trifecta_form_text_field.dart';
 
 class NewTaskForm extends StatefulWidget {
   const NewTaskForm({
     super.key,
+    required this.firebaseTaskListId,
   });
+
+  final String firebaseTaskListId;
 
   @override
   State<NewTaskForm> createState() => _NewTaskFormState();
@@ -15,12 +20,8 @@ class _NewTaskFormState extends State<NewTaskForm> {
 
   @override
   Widget build(BuildContext context) {
-    double deviceWidth = MediaQuery.of(context).size.width;
-    double deviceHeight = MediaQuery.of(context).size.height;
-
     return Container(
-      margin: EdgeInsets.symmetric(horizontal: deviceWidth * .10),
-      height: deviceHeight * .10,
+      height: 52,
       alignment: Alignment.center,
       child: Row(
         children: [
@@ -29,14 +30,32 @@ class _NewTaskFormState extends State<NewTaskForm> {
             color: Theme.of(context).colorScheme.primary,
             size: 20,
           ),
-          TrifectaFormTextField(
-            textInputType: TextInputType.text,
-            textEditingController: _titleController,
-            hintText: '//title..',
-            obsecureText: false,
+          Expanded(
+            child: Center(
+              child: TextField(
+                maxLines: 1,
+                maxLength: 20,
+                controller: _titleController,
+                style: Theme.of(context).textTheme.titleMedium,
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.zero,
+                  counter: const Offstage(),
+                  hintText: 'title...',
+                  hintStyle: Theme.of(context).textTheme.labelMedium,
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
           ),
           IconButton(
-            onPressed: () {},
+            onPressed: () {
+              context.read<TaskBloc>().add(
+                    CreateNewTaskEvent(
+                      taskTitle: _titleController.text.trim(),
+                      firebaseTaskListId: widget.firebaseTaskListId,
+                    ),
+                  );
+            },
             icon: Icon(
               Icons.check_rounded,
               size: 20,
