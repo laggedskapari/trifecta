b3b29e436ebf2c5b318cb964e4bb9f4e20e9b3a6
laggedskapari <es.rajatyadav@gmail.com>
2024-06-05 19:54:06 +0530

    added Task Repository.

--- lib/trifecta.dart
+++ lib/trifecta.dart
@@ -1,7 +1,9 @@
+import 'package:cross/cross_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:trifecta/bloc/Authentication/SignInBloc/sign_in_bloc.dart';
 import 'package:trifecta/bloc/Authentication/TrifectaAuthenticationBloc/trifecta_authentication_bloc.dart';
+import 'package:trifecta/bloc/Cross/TaskListBloc/tasklist_bloc.dart';
 import 'package:trifecta/presentation/color_scheme/trifecta_dark.dart';
 import 'package:trifecta/presentation/screens/authenticated/authenticated_screen.dart';
 import 'package:trifecta/presentation/screens/authentication/authentication_screen.dart';
@@ -11,9 +13,13 @@ class Trifecta extends StatelessWidget {
   const Trifecta({
     super.key,
     required this.trifectaAuthenticationRepository,
+    required this.crossTaskListRepository,
+    required this.crossTaskRepository,
   });
 
   final TrifectaAuthenticationRepository trifectaAuthenticationRepository;
+  final CrossTaskListRepository crossTaskListRepository;
+  final CrossTaskRepository crossTaskRepository;
 
   @override
   Widget build(BuildContext context) {
@@ -23,6 +29,17 @@ class Trifecta extends StatelessWidget {
           create: (context) => TrifectaAuthenticationBloc(
             trifectaAuthenticationRepository: trifectaAuthenticationRepository,
           ),
+        ),
+        RepositoryProvider(
+          create: (context) => TaskListBloc(
+            crossTaskListRepository: crossTaskListRepository,
+          ),
+        ),
+        RepositoryProvider(
+          create: (context) => crossTaskListRepository,
+        ),
+        RepositoryProvider(
+          create: (context) => crossTaskRepository,
         ),
       ],
       child: MaterialApp(
