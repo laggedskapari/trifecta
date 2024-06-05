275eedeab65eba8b92b79c720125d30d7f0a4a1d
laggedskapari <es.rajatyadav@gmail.com>
2024-06-05 19:54:39 +0530

    UI changed and added Bloc Provider.

--- lib/presentation/screens/authenticated/authenticated_screen.dart
+++ lib/presentation/screens/authenticated/authenticated_screen.dart
@@ -1,41 +1,50 @@
+import 'package:cross/cross_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:trifecta/bloc/Authentication/SignInBloc/sign_in_bloc.dart';
+import 'package:trifecta/bloc/Cross/TaskBloc/task_bloc.dart';
+import 'package:trifecta/bloc/Cross/TaskListBloc/tasklist_bloc.dart';
+import 'package:trifecta/presentation/components/authenticated/cross/cross_page.dart';
+import 'package:trifecta/presentation/components/authenticated/cross/cross_tasklist_list_view.dart';
+import 'package:trifecta/presentation/components/authenticated/cross/cross_tasks_list_view.dart';
+import 'package:trifecta/presentation/components/authenticated/cross/new_task_form.dart';
+import 'package:trifecta/presentation/components/authenticated/cross/new_tasklist_form.dart';
 import 'package:trifecta/presentation/components/authenticated/trifecta_navigation_bar.dart';
 import 'package:trifecta/presentation/components/trifecta_app_bar.dart';
 
-class AuthenticatedScreen extends StatelessWidget {
+class AuthenticatedScreen extends StatefulWidget {
   const AuthenticatedScreen({super.key});
 
+  @override
+  State<AuthenticatedScreen> createState() => _AuthenticatedScreenState();
+}
+
+class _AuthenticatedScreenState extends State<AuthenticatedScreen> {
   @override
   Widget build(BuildContext context) {
     void changeScreen(int x) {}
 
     return Scaffold(
       appBar: const TrifectaAppBar(),
-      body: Center(
-        child: Column(
-          children: [
-            const Text(
-              'Authenticated',
-            ),
-            TextButton(
-              onPressed: () {
-                context.read<SignInBloc>().add(const SignOutRequired());
-              },
-              child: const Text(
-                '[SIGNOUT]',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w900,
-                  fontFamily: 'JetBrainsMono',
-                ),
-              ),
-            )
-          ],
-        ),
+      body: MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create: (context) => TaskListBloc(
+                crossTaskListRepository:
+                    RepositoryProvider.of<CrossTaskListRepository>(context))
+              ..add(LoadTaskListsEvent()),
+          ),
+          BlocProvider(
+            create: (context) => TaskBloc(
+                crossTaskRepository:
+                    RepositoryProvider.of<CrossTaskRepository>(context)),
+          ),
+        ],
+        child: const CrossPage(),
       ),
-      bottomNavigationBar: TrifectaNavigationBar(changeScreen: changeScreen),
+      bottomNavigationBar: TrifectaNavigationBar(
+        changeScreen: changeScreen,
+      ),
     );
   }
 }
