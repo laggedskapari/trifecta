3831c77617aeb5b7f01bc78c6835a5f235a8e44d
laggedskapari <es.rajatyadav@gmail.com>
2024-06-07 13:50:07 +0530

    UI update.
    added new countdown widget.

--- lib/presentation/components/authenticated/cross/cross_page.dart
+++ lib/presentation/components/authenticated/cross/cross_page.dart
@@ -1,41 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:trifecta/presentation/components/authenticated/cross/cross_tasklist_list_view.dart';
 import 'package:trifecta/presentation/components/authenticated/cross/cross_tasks_list_view.dart';
+import 'package:trifecta/presentation/components/authenticated/cross/date_countdown_card.dart';
 
 class CrossPage extends StatelessWidget {
   const CrossPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    String firebaseTaskListId = '';
-
-    void loadTask(String firebaseTaskListId) {
-      firebaseTaskListId = firebaseTaskListId;
-    }
-
     final deviceHeight = MediaQuery.of(context).size.height;
     final deviceWidth = MediaQuery.of(context).size.width;
 
     return Container(
       margin: EdgeInsets.symmetric(horizontal: deviceWidth * 0.05),
-      child: Column(
+      child: const Column(
         children: [
           SizedBox(
             height: 30,
-            child: CrossTaskListListView(
-              loadTaskList: loadTask,
-            ),
+            child: CrossTaskListListView(),
           ),
-          SizedBox(
-            height: deviceHeight * 0.75,
-            child: CrossTasksListView(
-              firebaseTaskListId: firebaseTaskListId,
-            ),
+          DateCountdownCard(),
+          Expanded(
+            child: CrossTasksListView(),
           ),
         ],
       ),
     );
   }
 }
-
-
