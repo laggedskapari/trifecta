85433b0c2768209bd17d906cd0e6ee875d23bc25
laggedskapari <es.rajatyadav@gmail.com>
2024-06-07 13:51:20 +0530

    UI and logic update.

--- lib/presentation/components/confirm_dialog_box.dart
+++ lib/presentation/components/confirm_dialog_box.dart
@@ -6,72 +6,84 @@ class ConfirmDialogBox extends StatelessWidget {
     required this.dialogTitle,
     required this.onAffirmative,
     required this.onNegative,
+    required this.dialogAction,
   });
 
+  final String dialogAction;
   final String dialogTitle;
   final void Function() onAffirmative;
   final void Function() onNegative;
 
   @override
   Widget build(BuildContext context) {
-    double displayHeight = MediaQuery.of(context).size.height;
+    double deviceHeight = MediaQuery.of(context).size.height;
     return Container(
       alignment: Alignment.center,
+      margin:  EdgeInsets.symmetric(
+        vertical: deviceHeight * .40,
+      ),
       decoration: BoxDecoration(
         color: Theme.of(context).colorScheme.surface,
       ),
-      height: 50,
-      margin: EdgeInsets.symmetric(
-        vertical: displayHeight * .40,
-      ),
       child: Container(
+        height: 80,
         alignment: Alignment.center,
-        decoration:
-            BoxDecoration(border: Border.all(color: Colors.red, width: 3)),
         child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
+            Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: Text(
+                '//$dialogAction',
+                style: Theme.of(context).textTheme.labelMedium,
+              ),
+            ),
             Text(
-              dialogTitle,
+              'task -> $dialogTitle',
               style: Theme.of(context).textTheme.titleMedium,
             ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextButton(
-                  style: ButtonStyle(
+            Expanded(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextButton(
+                    style: ButtonStyle(
+                        overlayColor: WidgetStateColor.resolveWith(
+                            (states) => Colors.transparent)),
+                    onPressed: onAffirmative,
+                    child: Text(
+                      '[AFFIRMTIVE]',
+                      style: TextStyle(
+                        color: Theme.of(context).colorScheme.primary,
+                        fontSize: 12,
+                        fontWeight: FontWeight.w900,
+                        fontFamily: 'JetBrainsMono',
+                      ),
+                    ),
+                  ),
+                  TextButton(
+                    style: ButtonStyle(
                       overlayColor: WidgetStateColor.resolveWith(
-                          (states) => Colors.transparent)),
-                  onPressed: onAffirmative,
-                  child: Text(
-                    '[AFFIRMTIVE]',
-                    style: TextStyle(
-                      color: Theme.of(context).colorScheme.primary,
-                      fontSize: 12,
-                      fontWeight: FontWeight.w900,
-                      fontFamily: 'JetBrainsMono',
+                          (states) => Colors.transparent),
+                    ),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text(
+                      '[NEGATIVE]',
+                      style: TextStyle(
+                        color: Theme.of(context).colorScheme.error,
+                        fontSize: 12,
+                        fontWeight: FontWeight.w900,
+                        fontFamily: 'JetBrainsMono',
+                      ),
                     ),
                   ),
-                ),
-                TextButton(
-                  style: ButtonStyle(
-                    overlayColor: WidgetStateColor.resolveWith(
-                        (states) => Colors.transparent),
-                  ),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: Text(
-                    '[NEGATIVE]',
-                    style: TextStyle(
-                      color: Theme.of(context).colorScheme.error,
-                      fontSize: 12,
-                      fontWeight: FontWeight.w900,
-                      fontFamily: 'JetBrainsMono',
-                    ),
-                  ),
-                ),
-              ],
+                ],
+              ),
             )
           ],
         ),
@@ -79,4 +91,3 @@ class ConfirmDialogBox extends StatelessWidget {
     );
   }
 }
-
