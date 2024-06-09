92e3a621df0d092ff402035a5b6a72cd72b97b6b
laggedskapari <es.rajatyadav@gmail.com>
2024-06-09 11:44:44 +0530

    added log repository.
    exports all logs related dart files.

---
+++ packages/logs/lib/logs_repository.dart
@@ -0,0 +1,10 @@
+library logs_repository;
+
+export 'src/model/models.dart';
+export 'src/entity/entities.dart';
+export 'src/logs_log_repository.dart';
+export 'src/logs_log_repository_implementation.dart';
+export 'src/logs_task_repository.dart';
+export 'src/logs_task_repository_implementation.dart';
+export 'src/log_record_repository.dart';
+export 'src/log_record_repository_implementation.dart';
