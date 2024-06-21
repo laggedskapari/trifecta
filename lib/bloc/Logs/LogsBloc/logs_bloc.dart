da025cb881c053d14aebfc7520993076dd86fc78
laggedskapari <es.rajatyadav@gmail.com>
2024-06-22 02:11:11 +0530

    update: UI

--- lib/bloc/Logs/LogsBloc/logs_bloc.dart
+++ lib/bloc/Logs/LogsBloc/logs_bloc.dart
@@ -8,9 +8,31 @@ part 'logs_state.dart';
 class LogsBloc extends Bloc<LogsEvent, LogsState> {
   final LogsLogRepository logsLogRepository;
 
-  LogsBloc({required this.logsLogRepository}) : super(const LogsState.initiate()) {
-    on<LoadLogsEvent>((event, emit) {});
-    on<CreateNewLogEvent>((event, emit) {});
+  LogsBloc({required this.logsLogRepository})
+      : super(const LogsState.initiate()) {
+    on<LoadLogsEvent>((event, emit) async {
+      try {
+        emit(const LogsState.processing());
+        final logs = await logsLogRepository.getAllLogs().first;
+        emit(LogsState.success(logs: logs));
+      } catch (e) {
+        emit(LogsState.failure(errorMessage: e.toString()));
+      }
+    });
+
+    on<CreateNewLogEvent>((event, emit) async {
+      try {
+        await logsLogRepository.createNewLogsLog(
+          logTitle: event.logTitle,
+          logDuration: event.logDuration,
+          logTasks: event.logTasks,
+        );
+      } catch (e) {
+        print(e.toString());
+        print('You messed up!');
+
+      }
+    });
     on<DeleteLogEvent>((event, emit) {});
   }
 }
