5953c70319d5d6ad2feb77485de1a568099d3083
laggedskapari <es.rajatyadav@gmail.com>
2024-06-23 04:34:34 +0530

    update: Bloc refactor.

--- lib/bloc/Logs/LogsBloc/logs_state.dart
+++ lib/bloc/Logs/LogsBloc/logs_state.dart
@@ -1,22 +1,22 @@
 part of 'logs_bloc.dart';
 
-enum LogStatus { initiate, processing, success, failure }
+enum LogsStatus { initiate, processing, success, failure }
 
 final class LogsState extends Equatable {
   final List<LogsLog> logs;
   final String? error;
-  final LogStatus status;
+  final LogsStatus status;
 
   const LogsState._({
-    this.status = LogStatus.initiate,
+    this.status = LogsStatus.initiate,
     this.logs = const [],
     this.error,
   });
 
   const LogsState.initiate() : this._();
-  const LogsState.processing() : this._(status: LogStatus.processing);
-  const LogsState.success({required List<LogsLog> logs}) : this._(status: LogStatus.success, logs: logs);
-  const LogsState.failure({required String errorMessage}) : this._(status: LogStatus.failure, error: errorMessage);
+  const LogsState.processing() : this._(status: LogsStatus.processing);
+  const LogsState.success({required List<LogsLog> logs}) : this._(status: LogsStatus.success, logs: logs);
+  const LogsState.failure({required String errorMessage}) : this._(status: LogsStatus.failure, error: errorMessage);
 
   @override 
   List<Object?> get props => [logs, error, logs];
