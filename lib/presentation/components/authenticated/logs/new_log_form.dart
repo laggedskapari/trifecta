88f9eb95553a874c79ea4ac04d40b1128270e2da
laggedskapari <es.rajatyadav@gmail.com>
2024-06-30 20:28:42 +0530

    updated: UI for new_log_form

--- lib/presentation/components/authenticated/logs/new_log_form.dart
+++ lib/presentation/components/authenticated/logs/new_log_form.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:log/logs_repository.dart';
-import 'package:trifecta/bloc/Logs/LogsBloc/logs_bloc.dart';
 
 class NewLogForm extends StatefulWidget {
   const NewLogForm({super.key});
@@ -14,36 +11,21 @@ class NewLogForm extends StatefulWidget {
 class _NewLogFormState extends State<NewLogForm> {
   List<LogTask> logTasks = [];
 
-  void addLogTask() {
-    setState(() {
-      LogTask newLogTask = LogTask(
-        logTaskTitle: _logTaskTitleController.text.trim(),
-        firebaseLogTaskId: '',
-      );
-      logTasks.add(newLogTask);
-    });
-  }
-
-  void submitNewLog() {
-    BlocProvider.of<LogsBloc>(context).add(CreateNewLogEvent(logTitle: 'BE BETTER', logTasks: logTasks, logDuration: 10));
-  }
-
-  final _titleController = TextEditingController();
-  final _durationController = TextEditingController();
+  final _logTitleController = TextEditingController();
+  final _logDurationController = TextEditingController();
   final _logTaskTitleController = TextEditingController();
-
   @override
   Widget build(BuildContext context) {
     final deviceHeight = MediaQuery.of(context).size.height;
     final deviceWidth = MediaQuery.of(context).size.width;
 
-    return Material(
-      child: Container(
+    return Scaffold(
+      body: Container(
         margin: EdgeInsets.symmetric(
+          vertical: deviceHeight * .05,
           horizontal: deviceWidth * .05,
-          vertical: deviceHeight * .05
         ),
-        alignment: Alignment.center,
+        alignment: Alignment.centerLeft,
         child: Column(
           children: [
             Text(
@@ -51,128 +33,109 @@ class _NewLogFormState extends State<NewLogForm> {
               style: Theme.of(context).textTheme.titleLarge,
             ),
             TextField(
-              controller: _titleController,
-              keyboardType: TextInputType.text,
+              maxLines: 1,
+              maxLength: 20,
+              controller: _logTitleController,
+              style: Theme.of(context).textTheme.titleMedium,
               decoration: InputDecoration(
                 contentPadding: EdgeInsets.zero,
                 counter: const Offstage(),
-                hintText: 'title...',
-                hintStyle: Theme.of(context).textTheme.labelLarge,
+                hintText: 'log title...',
+                hintStyle: Theme.of(context).textTheme.labelMedium,
                 border: InputBorder.none,
               ),
             ),
             TextField(
-              controller: _durationController,
+              maxLines: 1,
+              maxLength: 20,
+              controller: _logTitleController,
               keyboardType: TextInputType.number,
-              inputFormatters: [
-                FilteringTextInputFormatter.digitsOnly,
-              ],
+              style: Theme.of(context).textTheme.titleMedium,
               decoration: InputDecoration(
                 contentPadding: EdgeInsets.zero,
                 counter: const Offstage(),
-                hintText: 'duration...',
-                hintStyle: Theme.of(context).textTheme.labelLarge,
+                hintText: 'log duration...',
+                hintStyle: Theme.of(context).textTheme.labelMedium,
                 border: InputBorder.none,
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    '//LOG TASKS',
-                    style: Theme.of(context).textTheme.labelLarge,
-                  ),
-                  Row(
-                    children: [
-                      Expanded(
-                        child: Text(
-                          'These tasks will be available daily. In order to extend your streak completed these tasks every day.',
-                          style: Theme.of(context).textTheme.labelSmall,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
+            Text(
+              '//LOG TASKS',
+              style: Theme.of(context).textTheme.labelLarge,
+            ),
+            Text(
+              'This is description for log tasks.',
+              style: Theme.of(context).textTheme.labelSmall,
             ),
             Expanded(
               child: ListView.builder(
                 itemBuilder: (context, index) => Text(
-                  '-> ${logTasks[index].logTaskTitle}',
+                  logTasks[index].logTaskTitle,
                   style: Theme.of(context).textTheme.titleMedium,
                 ),
                 itemCount: logTasks.length,
               ),
             ),
-            SizedBox(
-              height: 49,
-              child: Row(
-                children: [
-                  Icon(
-                    size: 20,
-                    Icons.double_arrow_rounded,
-                    color: Theme.of(context).colorScheme.primary,
-                  ),
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 8.0,
-                      ),
-                      child: TextField(
-                        controller: _logTaskTitleController,
-                        keyboardType: TextInputType.text,
-                        decoration: InputDecoration(
-                          contentPadding: EdgeInsets.zero,
-                          counter: const Offstage(),
-                          hintText: 'task title...',
-                          hintStyle: Theme.of(context).textTheme.labelMedium,
-                          border: InputBorder.none,
-                        ),
-                        maxLines: 1,
-                        maxLength: 20,
-                      ),
-                    ),
-                  ),
-                  IconButton(
-                    onPressed: addLogTask,
-                    icon: const Icon(
-                      Icons.check_rounded,
-                      size: 20,
-                      color: Colors.green,
-                    ),
-                  )
-                ],
-              ),
-            ),
             Row(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                TextButton(
-                  onPressed: submitNewLog,
-                  child: Text(
-                    '[PROCEED]',
-                    style: TextStyle(
-                      fontSize: 15,
-                      fontWeight: FontWeight.w900,
-                      color: Theme.of(context).colorScheme.primary,
+                Icon(
+                  Icons.double_arrow_rounded,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 4.0,
+                    ),
+                    child: TextField(
+                      maxLines: 1,
+                      maxLength: 20,
+                      controller: _logTitleController,
+                      keyboardType: TextInputType.number,
+                      style: Theme.of(context).textTheme.titleMedium,
+                      decoration: InputDecoration(
+                        contentPadding: const EdgeInsets.only(top: 4),
+                        counter: const Offstage(),
+                        hintText: 'log task title...',
+                        hintStyle: Theme.of(context).textTheme.labelMedium,
+                        border: InputBorder.none,
+                      ),
                     ),
                   ),
                 ),
-                TextButton(
+                IconButton(
                   onPressed: () {},
-                  child: Text(
-                    '[DECLINE]',
-                    style: TextStyle(
-                      fontSize: 15,
-                      fontWeight: FontWeight.w900,
-                      color: Theme.of(context).colorScheme.error,
-                    ),
+                  icon: Icon(
+                    Icons.add_task_rounded,
+                    color: Theme.of(context).colorScheme.primary,
                   ),
                 )
               ],
+            ),
+            Container(
+              alignment: Alignment.center,
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      '[PROCEED]',
+                      style: Theme.of(context).textTheme.titleMedium,
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text(
+                      '[DECLINE]',
+                      style: Theme.of(context).textTheme.titleMedium,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
